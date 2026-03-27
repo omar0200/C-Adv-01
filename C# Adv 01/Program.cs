be423f9076fd0a8eef206a303ee9cb59ecc31c34
omar0200 <omarminato17@gmail.com>
2026-03-27 15:04:55 +0200

    Question 1

--- C# Adv 01/Program.cs
+++ C# Adv 01/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1
+            //generic class is class that can work with any data type 
+            //we are using generic because its save and it has good preformance 
+            #endregion
         }
     }
 }
