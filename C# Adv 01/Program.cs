dcb9ed5d55ff5441d2bcd5bb49d92d4c1dc966d6
omar0200 <omarminato17@gmail.com>
2026-03-27 16:37:06 +0200

    Question 10

--- C# Adv 01/Program.cs
+++ C# Adv 01/Program.cs
@@ -21,6 +21,21 @@
             // Struct constrain force the parameter to be value type 
             Question_7<int> q = new Question_7<int>();
             #endregion
+
+            #region Question 8
+            // class constrain force the parameter to be refernce type 
+            Question8<string> p = new Question8<string>();
+            #endregion
+
+            #region Question 9
+            // must have parameterless constractor 
+            Question_9<int> q9 = new Question_9<int>(5);
+            #endregion
+
+            #region Question 10 
+            //it means class must implement the interface and the interface is generic and it can work with any data type 
+            Question_10<IRepository<string>> q10 = new Question_10<IRepository<string>>();  
+            #endregion
         }
     }
 }
---
+++ C# Adv 01/Question10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Adv_01
+{
+    internal class Question10<T> : IRepository<T> where T : interface
+    {
+        public void Update(T e)
+        {
+            throw new NotImplementedException();
+        }
+    
+}
+}
